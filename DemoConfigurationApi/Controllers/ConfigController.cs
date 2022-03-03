f3ae9bedda7894536d18064486463df01d807e6f
Kyle Alexander <mcphylus@live.co.uk>
2022-03-03 20:29:42 +0000

    Api and json changes

---
+++ DemoConfigurationApi/Controllers/ConfigController.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Monitoring.Abstractions;
+
+namespace DemoConfigurationApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ConfigController : ControllerBase
+{
+    private readonly IMonitor<ConfigController> _monitor;
+
+    public ConfigController(IMonitor<ConfigController> monitor)
+    {
+        _monitor = monitor;
+    }
+
+    [HttpGet(Name = "GetConfig")]
+    public object Get([FromQuery][Required] string service)
+    {
+        var result = new Dictionary<string, dynamic>();
+        IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.AddJsonFile("config.json");
+        configurationBuilder.AddJsonFile($"config.{service}.json", true);
+
+        return BuildValue(configurationBuilder.Build());
+    }
+
+    private object BuildValue(IConfiguration config)
+    {
+        var items = config.GetChildren();
+
+        {
+            var result = new List<object>();
+            if (items.First().Key == "0")
+            {
+                foreach (var item in items)
+                {
+                    result.Add(BuildValueFromSection(item));
+                }
+
+                return result;
+            }
+        }
+
+        {
+            var result = new Dictionary<string, object>();
+            foreach (var item in items)
+            {
+                result[item.Key] = BuildValueFromSection(item);
+            }
+
+            return result;
+        }
+    }
+
+    private object BuildValueFromSection(IConfigurationSection config)
+    {
+        if (config.Value is not null) return config.Value;
+
+        return BuildValue(config);
+    }
+}
---
+++ DemoConfigurationApi/Program.cs
@@ -0,0 +1,22 @@
+﻿using Monitoring;
+using Monitoring.Abstractions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.RegisterMonitoring(ConsoleFormat.Json);
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
